c20b18bd61d7660a44029755e6c9f1eed5e1c168
ogabriel <gabrieloliver8991@gmail.com>
2023-10-14 14:11:07 -0300

    update pessoa model

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
 class Pessoa < ApplicationRecord
+  before_save :build_busca
+
+  validate :valid_nascimento?
+
+  private
+
+  def valid_nascimento?
+    Date.parse(nascimento)
+  rescue Date::Error
+    errors.add(:nascimento, 'invalid')
+  end
+
+  def build_busca
+    return if errors.any?
+
+    self.busca = if stack.present?
+                   "#{apelido.downcase} #{nome.downcase} #{stack.map(&:downcase).join(' ')}"
+                 else
+                   "#{apelido.downcase} #{nome.downcase}"
+                 end
+  end
 end
