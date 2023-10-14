ea4121a55f02553c6bc7bf72082d9609062a6046
ogabriel <gabrieloliver8991@gmail.com>
2023-10-14 15:28:34 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pessoas, only: %i[show create]
-  get "pessoas" => "pessoas#search"
-  get "contagem-pessoas" => "pessoas#count"
+  get 'pessoas' => 'pessoas#search'
+  get 'contagem-pessoas' => 'pessoas#count'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
