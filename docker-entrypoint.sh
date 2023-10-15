65eb1e8883011096db11fb7a19bdecaf6246d2f4
ogabriel <gabrieloliver8991@gmail.com>
2023-10-14 22:56:42 -0300

    docker-entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$1" = 'dev' ]; then
+    cd /app
+    bundle install
+    exec "$2"
+elif [ "$1" = 'server' ]; then
+    cd app
+    exec /app/bin/rails server
+elif [ "$1" = 'migrate_and_server' ]; then
+    cd app
+    /app/bin/rails db:create db:migrate
+    exec /app/bin/rails server
+fi
