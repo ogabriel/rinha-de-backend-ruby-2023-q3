82277b916f1de2e0644a77f1e18cd77fcf9c520f
ogabriel <gabrieloliver8991@gmail.com>
2024-01-14 12:41:06 -0300

    start agoo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,10 +7,10 @@ if [ "$1" = 'dev' ]; then
 elif [ "$1" = 'server' ]; then
     cd /app
     make database-check
-    exec /app/bin/rails server
+    exec /app/bin/rails server -p $PORT -b 0.0.0.0 --environment=production --no-log-to-stdout
 elif [ "$1" = 'migrate_and_server' ]; then
     cd /app
     make database-check
     /app/bin/rails db:drop db:create db:migrate
-    exec /app/bin/rails server
+    exec /app/bin/rails server -p $PORT -b 0.0.0.0 --environment=production --no-log-to-stdout
 fi
